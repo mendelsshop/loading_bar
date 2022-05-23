cbd129565bb4789009ddec6fddd848ebd4267e8a
Mendel Rubin <82669516+mendelsshop@users.noreply.github.com>
2022-05-23 15:24:56 -0400

    maybe this

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,7 @@ impl LoadingBar {
     }
 
     pub fn advance(self: &mut LoadingBar) {
-        self.bar = self.bar.replacen(' ', "\u{25A0}", 1);
+        self.bar = self.bar.replacen(' ', "\u{2589}", 1);
         self.index += 1; // increment
         if self.index == self.len {
             self.done = true;
@@ -33,7 +33,7 @@ impl LoadingBar {
     }
 
     pub fn advance_by(self: &mut LoadingBar, index: u64) {
-        self.bar = self.bar.replacen(' ', "\u{25A0}", index as usize);
+        self.bar = self.bar.replacen(' ', "\u{2589}", index as usize);
         self.index += index; // increment
         if self.index >= self.len {
             self.done = true;
@@ -43,11 +43,11 @@ impl LoadingBar {
     pub fn advance_by_percent(self: &mut LoadingBar, percentage: f64) {
         let index = (self.len as f64 * percentage / 100.0) as u64;
         let reminder = (self.len as f64 * percentage % 100.0) as u64;
-        self.bar = self.bar.replacen(' ', "\u{25A0}", index as usize);
+        self.bar = self.bar.replacen(' ', "\u{2589}", index as usize);
         match reminder {
             0=> {},
             _ => {
-                self.bar = self.bar.replacen(' ', "\u{25E3}", 1);
+                self.bar = self.bar.replacen(' ', "\u{258c}", 1);
             }
         }
         self.index += index; // increment
