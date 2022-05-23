67fc4ba4cb541f599b1e2464bd8cea1009baee3e
Mendel Rubin <82669516+mendelsshop@users.noreply.github.com>
2022-05-23 00:59:18 -0400

    did color and blocks

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 use std::fmt;
+extern crate colored;
+
+use colored::*;
 
 #[derive(Debug)]
 pub struct LoadingBar {
@@ -6,19 +9,24 @@ pub struct LoadingBar {
     pub len: u64,
     index: u64,
     pub done: bool,
+    color: Option<colored::Color>,
 }
 impl LoadingBar {
-    pub fn new(len: u64) -> LoadingBar {
+    pub fn new(len: u64, color: Option<colored::Color>) -> LoadingBar {
         LoadingBar {
             bar: string_from_u64(len, ' '),
             len,
             index: 0,
             done: false,
+            color: match color {
+                Some(c) => Some(c),
+                _ => None,
+            },
         }
     }
 
     pub fn advance(self: &mut LoadingBar) {
-        self.bar = self.bar.replacen(' ', "#", 1);
+        self.bar = self.bar.replacen(' ', "\u{25A0}", 1);
         self.index += 1; // increment
         if self.index == self.len {
             self.done = true;
@@ -26,7 +34,7 @@ impl LoadingBar {
     }
 
     pub fn advance_by(self: &mut LoadingBar, index: u64) {
-        self.bar = self.bar.replacen(' ', "#", index as usize);
+        self.bar = self.bar.replacen(' ', "\u{25A0}", index as usize);
         self.index += index; // increment
         if self.index == self.len {
             self.done = true;
@@ -38,11 +46,15 @@ impl LoadingBar {
         // and then we have to get the index of the bar
         // that we want to advance
         let index = (self.len as f64 * percentage / 100.0) as u64;
-        self.bar = self.bar.replacen(' ', "#", index as usize);
+        self.bar = self.bar.replacen(' ', "\u{25A0}", index as usize);
         self.index += index; // increment
         if self.index == self.len {
             self.done = true;
         }
+    }
+
+    pub fn change_color(self: &mut LoadingBar, color: colored::Color) {
+        self.color = Some(color);
     }
 }
 impl fmt::Display for LoadingBar {
@@ -51,7 +63,11 @@ impl fmt::Display for LoadingBar {
     //         write!(f, " {0} \n[{1}]\n {2} ", string_from_u64(self.len, '\u{2581}'), self.bar, string_from_u64(self.len, '\u{2594}'))
     //     }
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "\r[{}]", self.bar)
+        write!(
+            f,
+            "\r[{}]",
+            self.bar.color(self.color.unwrap_or(colored::Color::White))
+        )
     }
 }
 fn string_from_u64(len: u64, replace: char) -> String {
--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,22 @@ use loading_bar::LoadingBar;
 use std::{io::Write, thread, time};
 
 fn main() {
-    let mut bar = LoadingBar::new(100);
+    let mut bar = LoadingBar::new(50, Some(colored::Color::Green));
 
     print!("\r{}", bar);
     std::io::stdout().flush().unwrap();
+    // flush stdout or else wont work properly
+
     for _i in 0..5 {
         if bar.done {
+            // we dont want to advance the bar if it is done
             break;
         }
-        let ten_millis = time::Duration::from_secs(1);
-        thread::sleep(ten_millis);
-        bar.advance_by_percent(25.0);
+        let ten_millis = time::Duration::from_secs(3); // wow what a descriptive name
+        thread::sleep(ten_millis); // sleep for 1 seconds
+        bar.advance_by_percent(25.0); // advancing by 25%
         print!("\r{}", bar);
+        bar.change_color(colored::Color::Red);
         // flush stdout or else wont work properly
         std::io::stdout().flush().unwrap();
     }
